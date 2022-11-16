75fcc6f61476ea7635ce197d265e8b995c5a0201
Gulbahar Ergelen <bahar.ergelen@hotmail.com>
2022-11-16 22:01:34 +0300

    abc

--- work1.py
+++ work1.py
@@ -11,6 +11,25 @@ print(number)
 dnumber = 2,56
 print(dnumber)
 
+number = 30
+print(number + 8)
+print(number - 6)
+print(number * 3)
+print(number / 6)
+print(number % 7)
+print(type(number))
 
+text = "Etiya Akademi"
+print(text.upper())
+print(text.lower())
+
+mesaj ="Etiya Akademi"
+haber = "5.dönem"
+mesaj1 ="Hoşgeldin"
+name ="Gülbahar"
+
+print (f"{mesaj} {haber}'e {mesaj1} {name}")
+
+print("Tesekkürler")
 
  
